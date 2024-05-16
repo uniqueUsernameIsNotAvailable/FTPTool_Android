7fd66f438682a171fd773ad44b82486371382b05
Miaw <hexahexagon@mail.ru>
2024-05-17 00:18:33 +0300

    LIBS REWORK END. NO JAVA PRESENT

--- app/src/main/java/com/tyoma/testingzone/libs/user/MyFtpUser.kt
+++ app/src/main/java/com/tyoma/testingzone/libs/user/MyFtpUser.kt
@@ -1,41 +1,17 @@
-package com.tyoma.testingzone.libs.user;
+package com.tyoma.testingzone.libs.user
 
-public final class MyFtpUser {
-    private String name;
-    private String password;
-    private String sharedPath;
-    private MyFtpUserPerm permission;
-
-    public MyFtpUser(String name, String password, String sharedPath, MyFtpUserPerm permission) {
-        this.name = name;
-        this.password = password;
-        this.sharedPath = sharedPath;
-        this.permission = permission;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getSharedPath() {
-        return sharedPath;
-    }
-
-    public MyFtpUserPerm getPermission() {
-        return permission;
-    }
-
-    @Override
-    public String toString() {
+class MyFtpUser(
+    val name: String,
+    val password: String,
+    val sharedPath: String,
+    val permission: MyFtpUserPerm
+) {
+    override fun toString(): String {
         return "MyFtpUser{" +
                 "name='" + name + '\'' +
                 ", password='" + password + '\'' +
                 ", sharedPath='" + sharedPath + '\'' +
                 ", permission=" + permission +
-                '}';
+                '}'
     }
 }
--- app/src/main/java/com/tyoma/testingzone/libs/user/MyFtpUserPerm.kt
+++ app/src/main/java/com/tyoma/testingzone/libs/user/MyFtpUserPerm.kt
@@ -1,22 +1,8 @@
-package com.tyoma.testingzone.libs.user;
+package com.tyoma.testingzone.libs.user
 
-import androidx.annotation.NonNull;
+import org.apache.ftpserver.ftplet.Authority
+import org.apache.ftpserver.usermanager.impl.WritePermission
 
-import org.apache.ftpserver.ftplet.Authority;
-import org.apache.ftpserver.usermanager.impl.WritePermission;
-
-
-public enum MyFtpUserPerm {
-
-    WRITE(new WritePermission());
-
-    private Authority authority;
-
-    public Authority getAuthority() {
-        return authority;
-    }
-
-    MyFtpUserPerm(@NonNull Authority authority) {
-        this.authority = authority;
-    }
+enum class MyFtpUserPerm(val authority: Authority) {
+    WRITE(WritePermission())
 }
